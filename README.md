fcf10b456ace76b4c5e6c391ab594004d1586713
Victor Kareh <vkareh@vkareh.net>
2014-11-05 11:59:10 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Mongoose listenTo
+=================
+
+[Mongoose listenTo](https://github.com/seelio/mongoose-listento) is a
+[mongoose](http://mongoosejs.com/) plugin that tells a schema to listen to
+events from other models. This module is heavily based on
+[Backbone.js Events](http://backbonejs.org/#Events).
+
+Usage
+-----
+After defining your schema, load the plugin:
+```javascript
+var listenTo = require('mongoose-listento');
+var MySchema = new mongoose.Schema({ ... });
+MySchema.plugin(listenTo);
+```
+
+Now you are ready to start listening to models from your schema in a way similar
+to using `pre` and `post` hooks:
+```javascript
+MySchema.listenTo(OtherModel, 'add', function(otherModelInstance) {
+  // `this` is bound to MySchema's Model
+});
+```
+
+This plugin provides the following schema methods:
+
+* `listenTo`: Inversion-of-control version of `on`. Tell *this* schema to listen to an event in another model.
+* `listenToOnce`: Inversion-of-control version of `once`. Tell *this* schema to listen once to an event in another model.
+* `stopListening`: Tell this schema to stop listening to either specific events or to every object it's currently listening to.
+
+You can emit any custom event from within the schema (or from the model itself
+by calling `MyModel.schema.emit( ... )`). By default, this plugin emits the
+following events:
+
+* `add`: When a new model is saved to the database.
+* `change`: When a model is updated.
+* `change:[attribute]`: When a model attribute is updated. Sends specific attribute to the callback.
+* `remove`: When a model is removed from the database.
---
+++ test/mongoose-listento.js
@@ -0,0 +1,114 @@
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var listento = require('../lib/mongoose-listento');
+var _ = require('underscore');
+
+var StudentSchema, Student, student, UniversitySchema, University, university;
+describe('Mongoose ListenTo plugin', function() {
+  before(function(done) {
+    // Connect to new test database
+    mongoose.connect('mongodb://localhost/mongoose-listento-test', done);
+  });
+
+  before(function() {
+    // Define schemas
+    StudentSchema = mongoose.Schema({ name: String, _university: {
+      type: mongoose.Schema.ObjectId, ref: 'University'
+    }});
+    UniversitySchema = mongoose.Schema({ name: String, city: String, departments: [String] });
+  });
+
+  before(function() {
+    // Add plugin
+    StudentSchema.plugin(listento);
+    UniversitySchema.plugin(listento);
+  });
+
+  before(function() {
+    // Instantiate models
+    Student = mongoose.model('Student', StudentSchema);
+    University = mongoose.model('University', UniversitySchema);
+  });
+
+  it('should create a new model and trigger an `add` event', function(done) {
+    StudentSchema.listenToOnce('University', 'add', function(u) {
+      expect(this).to.equal(Student);
+      expect(u).to.be.an.instanceof(University);
+      expect(u.name).to.equal('University of Michigan');
+      expect(u.city).to.equal('Ann Arbor');
+      done();
+    });
+    university = new University({name: 'University of Michigan', city: 'Ann Arbor'});
+    university.save();
+  });
+
+  it('should create model and trigger an `add` event', function(done) {
+    UniversitySchema.listenToOnce('Student', 'add', function(s) {
+      expect(this).to.equal(University);
+      expect(s).to.be.an.instanceof(Student);
+      expect(s.name).to.equal('Victor');
+      expect(s._university).to.equal(university._id);
+      done();
+    });
+    student = new Student({name: 'Victor', _university: university._id});
+    student.save();
+  });
+
+  it('should update model and trigger `change` events', function(done) {
+    var remaining = 2;
+    StudentSchema.listenTo('University', 'change', function(u) {
+      expect(this).to.equal(Student);
+      expect(u).to.be.an.instanceof(University);
+      expect(u.city).to.equal('Ann Arbor, Michigan');
+      if (--remaining === 0) {
+        StudentSchema.stopListening(University, 'change');
+        done();
+      }
+    });
+    university.city = 'Ann Arbor, Michigan';
+    university.save(function() {
+      university.name = 'U of M';
+      university.save();
+    });
+  });
+
+  it('should update model and trigger a `change:[attribute]` event', function(done) {
+    UniversitySchema.listenToOnce('Student', 'change:name', function(s, name) {
+      expect(this).to.equal(University);
+      expect(s).to.be.an.instanceof(Student);
+      expect(s.name).to.equal('Victor Kareh');
+      expect(s._university).to.equal(university._id);
+      done();
+    });
+    student.name = 'Victor Kareh';
+    student.save();
+  });
+
+  it('should stop listening to an event', function(done) {
+    var _fail = function() {
+      expect(true).to.be(false);
+    };
+    StudentSchema.listenTo('University', 'add', _fail);
+    StudentSchema.stopListening('University', 'add', _fail);
+    university = new University({name: 'MIT', city: 'Cambridge, Massachusetts'});
+    university.save(done);
+  });
+
+  it('should delete a model and trigger a `remove` event', function(done) {
+    StudentSchema.listenToOnce('University', 'remove', function(u) {
+      expect(this).to.equal(Student);
+      expect(u).to.be.an.instanceof(University);
+      expect(u._id).to.equal(university._id);
+      University.count({_id: u._id}, function(err, count) {
+        expect(count).to.equal(0);
+        done(err);
+      });
+    });
+    university.remove();
+  });
+
+  after(function(done) {
+    // Drop test database
+    mongoose.connection.db.dropDatabase(done);
+  });
+});
